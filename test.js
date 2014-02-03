137b7c7a85015329ee1c596d1625495815d42f2b
Tim Olsen <olse.n.tim@gmail.com>
2014-02-03 14:44:29 -0700

    1st committ
    First committ. This is basically sheetsee-cache repository with changes
    to make producebuddy. Runs node.js and tabletop.js

---
+++ test.js
@@ -0,0 +1,5 @@
+var SheetSee = require('./')
+
+var URL = "https://docs.google.com/spreadsheet/pub?key=0Akrw1GWTSr__dG5wNWZhZUdDR3NKTnNRSDFsYjBNcmc&single=true&gid=0&output=html"
+
+new SheetSee(URL)
